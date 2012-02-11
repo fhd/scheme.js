3c300e6c3ae92f5ef0f248bad75c4429a9a786c6
Felix H. Dahlke <fhd@ubercode.de>
2012-02-11 09:13:49 +0100

    Implement + and a basic REPL

--- scheme.js
+++ scheme.js
@@ -1,8 +1,53 @@
 var scheme = {};
 
 (function(scheme) {
-    scheme.eval = function(code) {
-            return "Evaluating: " + code;
+    function tokenise(expression) {
+        return expression.match(/([()]|[^\s()]+)/g);
+    }
+
+    function readAtom(token) {
+        var number = parseFloat(token);
+        if (!isNaN(number))
+            return number;
+        return token;
+    }
+    
+    function read(tokens) {
+        if (tokens.length === 0)
+            throw "Unexpected EOF while reading";
+        var token = tokens[0];
+        tokens.shift();
+        if (token === "(") {
+            var list = [];
+            while (tokens[0] != ")")
+                list.push(read(tokens));
+            tokens.shift();
+            return list;
+        } else if (token === ")")
+            throw "Unexpected )";
+        else
+            return readAtom(token);
+    }
+
+    function parse(expression) {
+        return read(tokenise(expression));
+    }
+
+    function isArray(x) {
+        return Object.prototype.toString.call(x) === '[object Array]';
+    }
+
+    function eval(parseTree) {
+        if (!isArray(parseTree))
+            return parseTree;
+        var functionName = parseTree[0];
+        if (functionName === "+")
+            return eval(parseTree[1]) + eval(parseTree[2]);
+        throw "Unknown function: " + functionName;
+    }
+
+    scheme.eval = function(string) {
+        return eval(parse(string));
     };
 })(scheme);
 
