1b78e977a1fffad200363716f2fa3c2b5fdf9875
Felix H. Dahlke <fhd@ubercode.de>
2012-02-11 13:00:04 +0100

    Implement variables

--- scheme.js
+++ scheme.js
@@ -37,21 +37,32 @@ var scheme = {};
         return Object.prototype.toString.call(x) === '[object Array]';
     }
 
-    function eval(x) {
+    function eval(x, env) {
+        if (typeof x === "string")
+            return env[x];
         if (!isArray(x))
             return x;
         var functionName = x[0];
         if (functionName === "if")
-            return eval(eval(x[1]) ? x[2] : x[3]);
-        else if (["+", "-", "*", "/", "=", ">", "<", ">=", "<="]
-                 .indexOf(functionName) !== -1)
-            return window.eval(eval(x[1]) +
+            return eval(eval(x[1], env) ? x[2] : x[3], env);
+        else if (functionName == "define") {
+            env[x[1]] = eval(x[2], env);
+            return null;
+        } else if (functionName == "set!") {
+            var varName = x[1];
+            if (!(varName in env))
+                throw "Unbound variable: " + varName;
+            env[varName] = x[2];
+            return null;
+        } else if (["+", "-", "*", "/", "=", ">", "<", ">=", "<="]
+                   .indexOf(functionName) !== -1)
+            return window.eval(eval(x[1], env) +
                                (functionName === "=" ? "==" : functionName) +
-                               eval(x[2]));
+                               eval(x[2]), env);
         throw "Unknown function: " + functionName;
     }
 
-    scheme.eval = function(string) {
-        return eval(read(string));
+    scheme.eval = function(string, env) {
+        return eval(read(string), env);
     };
 })(scheme);
