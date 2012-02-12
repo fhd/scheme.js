91103c0436484e627c276bf30c1f41a500d27c0c
Felix H. Dahlke <fhd@ubercode.de>
2012-02-12 09:32:48 +0100

    Refactor reader

--- scheme.js
+++ scheme.js
@@ -1,6 +1,47 @@
 var scheme = {};
 
 (function(scheme) {
+    var readMacros = readMacros = {
+        "if": function(env, _, test, consequence, alternative) {
+            return eval(eval(test, env) ? consequence : alternative, env);
+        },
+        "define": function(env, _, name, value) {
+            env.set(name, eval(value, env));
+        },
+        "set!": function(env, _, name, value) {
+            if (!(name in env))
+                throw "Unbound variable: " + name;
+            env.set(name, value);
+        },
+        "lambda": function(env, _, lambdaArgs, body) {
+            return function() {
+                var argMap, i;
+                if (arguments.length !== lambdaArgs.length)
+                    throw "Invalid number of arguments. Expected " +
+                        lambdaArgs.length + ", got " + arguments.length;
+                argMap = {};
+                for (i = 0; i < lambdaArgs.length; i++)
+                    argMap[lambdaArgs[i]] = arguments[i];
+                return eval(body, new scheme.Environment(argMap, env));
+            };
+        },
+        "=": function(env, _, first, second) {
+            return eval(first, env) == eval(second, env);
+        }
+    };
+
+    function forEach(array, f) {
+        var i;
+        for (i = 0; i < array.length; i++)
+            f(array[i]);
+    }
+
+    forEach(["+", "-", "*", "/", ">", "<", ">=", "<="], function(operator) {
+        readMacros[operator] = function(env, operator, first, second) {
+            return window.eval(eval(first, env) + operator + eval(second, env));
+        };
+    });
+
     scheme.Environment = function(entries, outer) {
         this.entries = entries || {};
         this.outer = outer || null;
@@ -54,53 +95,31 @@ var scheme = {};
     }
 
     function map(array, f) {
-        var newArray = [], i;
+        var i, newArray = [];
         for (i = 0; i < array.length; i++)
             newArray.push(f(array[i]));
         return newArray;
     }
 
+    function callProcedure(x, env) {
+        var expressions = map(x, function(element) {
+                return eval(element, env);
+            }),
+            firstExpression = expressions[0];
+        expressions.shift();
+        return firstExpression.apply(null, expressions);
+    }
+
     function eval(x, env) {
-        var first, varName, expressions, firstExpression;
+        var readMacro, expressions, firstExpression;
         if (typeof x === "string")
             return env.get(x);
         if (!isArray(x))
             return x;
-        first = x[0];
-        if (first === "if")
-            return eval(eval(x[1], env) ? x[2] : x[3], env);
-        else if (first == "define") {
-            env.set(x[1], eval(x[2], env));
-            return null;
-        } else if (first == "set!") {
-            varName = x[1];
-            if (!(varName in env))
-                throw "Unbound variable: " + varName;
-            env.set(varName, x[2]);
-            return null;
-        } else if (first == "lambda") {
-            return function() {
-                var lambdaArgs = x[1], argumentMap;
-                if (arguments.length !== lambdaArgs.length)
-                    throw "Invalid number of arguments. Expected " +
-                        lambdaArgs.length + ", got " + arguments.length;
-                argumentMap = {};
-                for (var i = 0; i < lambdaArgs.length; i++)
-                    argumentMap[lambdaArgs[i]] = arguments[i];
-                return eval(x[2], new scheme.Environment(argumentMap, env));
-            };
-        } else if (["+", "-", "*", "/", "=", ">", "<", ">=", "<="]
-                   .indexOf(first) !== -1) {
-            return window.eval(eval(x[1], env) +
-                               (first === "=" ? "==" : first) +
-                               eval(x[2], env));
-        }
-        expressions = map(x, function(element) {
-            return eval(element, env);
-        });
-        firstExpression = expressions[0];
-        expressions.shift();
-        return firstExpression.apply(null, expressions);
+        readMacro = readMacros[x[0]];
+        if (readMacro)
+            return readMacro.apply(null, [env].concat(x));
+        return callProcedure(x, env);
     }
 
     scheme.eval = function(string, env) {
