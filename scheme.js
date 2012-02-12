106fe2ddb918402c1ae162c6c0f8ed87d7d145bc
Felix H. Dahlke <fhd@ubercode.de>
2012-02-12 08:14:11 +0100

    Implement lambdas

--- scheme.js
+++ scheme.js
@@ -1,6 +1,21 @@
 var scheme = {};
 
 (function(scheme) {
+    scheme.Environment = function(entries, outer) {
+        this.entries = entries || {};
+        this.outer = outer || null;
+    }
+
+    scheme.Environment.prototype = {
+        get: function(name) {
+            return this.entries[name]
+                || (this.outer ? this.outer.get(name) : null);
+        },
+        set: function(name, value) {
+            this.entries[name] = value;
+        }
+    };
+
     function tokenise(expression) {
         return expression.match(/([()]|[^\s()]+)/g);
     }
@@ -37,29 +52,53 @@ var scheme = {};
         return Object.prototype.toString.call(x) === '[object Array]';
     }
 
+    function map(array, f) {
+        var newArray = [];
+        for (var i = 0; i < array.length; i++)
+            newArray.push(f(array[i]));
+        return newArray;
+    }
+
     function eval(x, env) {
         if (typeof x === "string")
-            return env[x];
+            return env.get(x);
         if (!isArray(x))
             return x;
-        var functionName = x[0];
-        if (functionName === "if")
+        var first = x[0];
+        if (first === "if")
             return eval(eval(x[1], env) ? x[2] : x[3], env);
-        else if (functionName == "define") {
-            env[x[1]] = eval(x[2], env);
+        else if (first == "define") {
+            env.set(x[1], eval(x[2], env));
             return null;
-        } else if (functionName == "set!") {
+        } else if (first == "set!") {
             var varName = x[1];
             if (!(varName in env))
                 throw "Unbound variable: " + varName;
-            env[varName] = x[2];
+            env.set(varName, x[2]);
             return null;
+        } else if (first == "lambda") {
+            return function() {
+                var lambdaArgs = x[1];
+                if (arguments.length !== lambdaArgs.length)
+                    throw "Invalid number of arguments. Expected " +
+                        lambdaArgs.length + ", got " + arguments.length;
+                var argumentMap = {};
+                for (var i = 0; i < lambdaArgs.length; i++)
+                    argumentMap[lambdaArgs[i]] = arguments[i];
+                return eval(x[2], new scheme.Environment(argumentMap, env));
+            };
         } else if (["+", "-", "*", "/", "=", ">", "<", ">=", "<="]
-                   .indexOf(functionName) !== -1)
+                   .indexOf(first) !== -1) {
             return window.eval(eval(x[1], env) +
-                               (functionName === "=" ? "==" : functionName) +
-                               eval(x[2]), env);
-        throw "Unknown function: " + functionName;
+                               (first === "=" ? "==" : first) +
+                               eval(x[2], env));
+        }
+        var expressions = map(x, function(element) {
+                return eval(element, env);
+            }),
+            firstExpression = expressions[0];
+        expressions.shift();
+        return firstExpression.apply(null, expressions);
     }
 
     scheme.eval = function(string, env) {
