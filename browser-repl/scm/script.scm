44be2db48e5c8fbdd796ca5b9445cc8c2e026f1c
Felix H. Dahlke <fhd@ubercode.de>
2012-02-18 22:05:22 +0100

    Update browser-repl

--- browser-repl/scm/script.scm
+++ browser-repl/scm/script.scm
@@ -1,22 +1,18 @@
-(define (format result)
-  (if (array? result)
-      (reduce result (lambda (x y)
-		       (string-append x "\n" y)))
-      (string-append result "\n")))
+(define $ (.$ js))
 
-(.$ js
-    (lambda ()
-      (let ((env (new scheme.Environment))
-	    (.$ js "#shell"))
-	(.console
-	 shell
-	 (hash ((promptLabel "=> ")
-		(commandHandle (lambda (line)
-				 (if (= (get line length) 0)
-				     ""
-				     (try (format (.eval scheme line env))
-					  (catch e (string-append "Error: "
-								  e))))))
-		(autofocus #t)
-		(animateScroll #t)
-		(promptHistory #t)))))))
+($ (lambda ()
+     (let ((env (new (.Environment scheme)))
+           (shell ($ "#shell")))
+       ((.console shell)
+        (make-object
+         ((promptLabel "=> ")
+          (commandHandle (lambda (line)
+                           (if (.length line)
+                               (try
+                                ((.print scheme)
+                                 ((.eval scheme) ((.read scheme) line) env))
+                                (lambda (e) (string-append "Error: " e)))
+                               "")))
+          (autofocus #t)
+          (animateScroll #t)
+          (promptHistory #t)))))))
